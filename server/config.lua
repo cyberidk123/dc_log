51158a8e9048456965cf3c8379c3a8ab408a4b5c
cyberidk123 <imabotgames7@gmail.com>
2024-12-31 08:06:51 -0800

    oh ea

--- server/config.lua
+++ server/config.lua
@@ -1,10 +1,11 @@
 Config = {}
 
-Config.hLink = 'https://discord.com/api/webhooks/1323679154431987813/KXP8vksMvAwbt33HG9msdbh2fX2PMC6t6n__IsClrHzkJPfzk8dAQSd0kicvQypc7b3c'
+Config.whlink = 'https://discord.com/api/webhooks/1323679154431987813/KXP8vksMvAwbt33HG9msdbh2fX2PMC6t6n__IsClrHzkJPfzk8dAQSd0kicvQypc7b3c' -- Default Webhook Link
 
-Config.joinLeaveLog = true
-Config.joinLeaveLink = 'https://discord.com/api/webhooks/1323679154431987813/KXP8vksMvAwbt33HG9msdbh2fX2PMC6t6n__IsClrHzkJPfzk8dAQSd0kicvQypc7b3c'
+Config.loginLog = true -- Log when players join and leave?
+Config.loginLogLink = 'https://discord.com/api/webhooks/1323679154431987813/KXP8vksMvAwbt33HG9msdbh2fX2PMC6t6n__IsClrHzkJPfzk8dAQSd0kicvQypc7b3c' -- WebHook Link to log logis. (Only works when Confi.loginLog = true).
 
 
-Config.name = 'Dexter Logger'
-Config.logo = 'https://imgur.com/UbfG4Jo'
+Config.whName = 'Zetta64 Logger' -- Name of the WebHook
+Config.whLogo = 'https://imgur.com/UbfG4Jo.png' -- Avatar of the WebHook
+
--- server/server.lua
+++ server/server.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-function SendWebHook(link, title, color, message)
+function SendWebHook(whLink, title, color, message)
     local embedMsg = {}
     timestamp = os.date("%c")
     embedMsg = {
@@ -13,26 +13,26 @@ function SendWebHook(link, title, color, message)
             },
         }
     }
-    PerformHttpRequest(link,
-    function(err, text, headers)end, 'POST', json.encode({username = Config.name, avatar_url= Config.logo ,embeds = embedMsg}), { ['Content-Type']= 'application/json' })
+    PerformHttpRequest(whLink,
+    function(err, text, headers)end, 'POST', json.encode({username = Config.whName, avatar_url= Config.whLogo ,embeds = embedMsg}), { ['Content-Type']= 'application/json' })
 end
 
 
-AddEventHandler('z64_logs:sendWebhook', function(data)
-    if data.link == nil then
-        link = Config.hLink
+AddEventHandler('z64_logs:sendWebhook', function(whData)
+    if whData.link == nil then
+        whLink = Config.whlink
     else
-        link = data.link
+        whLink = whData.link
     end
-    title = data.title
-    color = data.color
-    message = data.message
-    SendWebHook(link, title, color, message)
+    title = whData.title
+    color = whData.color
+    message = whData.message
+    SendWebHook(whLink, title, color, message)
 end)
 
 Citizen.CreateThread(function()
-    if Config.joinLeaveLog then
-        if Config.joinLeaveLogLink == '' then
+    if Config.loginLog then
+        if Config.loginLogLink == '' then
             print('^7[^1INFO^7]: Please set a WebHook URL in the config.lua to log players joining and leaving.')
         else
             print('test 1')
@@ -41,7 +41,7 @@ Citizen.CreateThread(function()
             local ids = GetPlayerIdentifier(id, steam)
             local plyName = GetPlayerName(id)
             local whData = {
-                link = Config.joinLeaveLogLink,
+                link = Config.loginLogLink,
                 title = plyName.." JOINING",
                 color = 655104,
                 message = 
@@ -58,7 +58,7 @@ Citizen.CreateThread(function()
             local plyName = GetPlayerName(id)
             local reason = reason
             local whData = {
-                link = Config.joinLeaveLogLink,
+                link = Config.loginLogLink,
                 title = plyName.." LEFT",
                 color = 16711689,
                 message = 
@@ -75,12 +75,12 @@ end)
 AddEventHandler('onResourceStart', function(resource)
     resName = GetCurrentResourceName()
     if resource == resName then
-        if Config.hlink == '' then
+        if Config.whlink == '' then
             print('^7[^1INFO^7]: No default WebHook URL detected. Please configure the script correctly.')
         else 
             print('^7[^2INFO^7]: '..resName..' initiated succesfully.')
             local whData = {
-                link = Config.hlink,
+                link = Config.whlink,
                 title = "LOGGER STARTED",
                 color = 4521728,
                 message = 
